d9e7860ca38dc3803a665cd71174db8bd4d9606b
CodeSandbox <support@codesandbox.io>
2023-10-26 17:53:54 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,21 +18,17 @@ function App() {
       <h1>React + Vite</h1>
       <h2>On CodeSandbox!</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR.
         </p>
 
         <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
+          Tip: you can use the inspector button next to address bar to click on components
+          in the preview and open the code in the editor!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
